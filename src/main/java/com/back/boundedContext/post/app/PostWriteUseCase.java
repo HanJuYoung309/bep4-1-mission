c6896a8d543fb9674463c47ed1466d1b1720277b
HanJuYoung309 <juyg407@naver.com>
2026-09-18 12:13:27 +0900

    0009

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -9,34 +9,21 @@ import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class PostService {
+public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public long count() {
-        return postRepository.count();
-    }
-
     public Post write(Member author, String title, String content) {
-        //Post post = new Post(author, title, content);
         Post post = postRepository.save(new Post(author, title, content));
 
-        author.increaseActivityScore(3);
         eventPublisher.publish(
                 new PostCreatedEvent(
                         new PostDto(post)
                 )
         );
 
-        return postRepository.save(post);
-
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+        return post;
     }
 }
